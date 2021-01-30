ac99684670f2b2272ac12384c267a22efc896f95
Jérémy Guyot <jeremyguy@orange.fr>
2021-01-30 19:52:17 +0100

    Déplacé BridgeManager dans managers.py

---
+++ bridges/managers.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+from geo.utils import get_bounding_box
+
+
+class BridgeManager(models.Manager):
+    """Manager responsible to handle bridge search and creation."""
+
+    def get_or_create(
+        self, latitude, longitude, user, height=None, width=None
+    ):
+        """Records a new bridge if not already in the database."""
+        bounding_box = get_bounding_box(
+            latitude, longitude, settings.BOUNDING_BOX_HALF_SIDE
+        )
+        created = False
+        try:
+            # Search bridges whose bounding box include latitude and longitude
+            bridge = self.model.object.filter(
+                latitude_north__gt=latitude,
+                latitude_south__lt=latitude,
+                longitude_west__lt=longitude,
+                longitude_east__gt=longitude,
+            )
+            bridge.latitude = latitude
+            bridge.longitude = longitude
+            bridge.bbox = bounding_box
+        except self.model.DoesNotExist:
+            # Create a new bridge, since it does not exist
+            bridge = self.model.object.create(
+                latitude=latitude,
+                longitude=longitude,
+                height=height,
+                width=width,
+                **bounding_box,
+            )
+            created = True
+
+        # Update height and width if new ones are smaller
+        if bridge.height is None:
+            bridge.height = height
+        if bridge.width is None:
+            bridge.width = width
+
+        if height and settings.BRIDGE_MIN_HEIGHT <= height <= bridge.height:
+            bridge.height = height
+
+        if width and settings.BRIDGE_MIN_WIDTH <= width <= bridge.width:
+            bridge.width = width
+
+        # Add user in the update history of the current bright info
+        bridge.users.add(user)
+
+        bridge.save()
+        return bridge, created
