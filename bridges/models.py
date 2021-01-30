e3b176b5ec4bfc466a8c2deaeac14e867903409b
Jérémy Guyot <jeremyguy@orange.fr>
2021-01-30 18:17:55 +0100

    Modifié le modèle bridge

--- bridges/models.py
+++ bridges/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.conf import settings
 
 
+class BridgeManager(models.Manager):
+    def get_or_create(self, latitude, longitude, height, width):
+        pass
+
+
 class Bridge(models.Model):
     height = models.DecimalField(
         max_digits=4,
@@ -15,6 +20,22 @@ class Bridge(models.Model):
         blank=True,
         null=True,
     )
+    latitude_north = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+    )
+    latitude_south = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+    )
+    longitude_west = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+    )
+    longitude_west = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+    )
     latitude = models.DecimalField(
         max_digits=10,
         decimal_places=7,
@@ -23,35 +44,38 @@ class Bridge(models.Model):
         max_digits=10,
         decimal_places=7,
     )
-    creator = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name="bridges",
-    )
-    last_contributor = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name="last_contributor_bridges",
-    )
     created_date = models.DateTimeField(
         auto_now_add=True,
     )
     last_update = models.DateTimeField(
         auto_now=True,
     )
-
-class Assobridge(models.Model):
-
-    user = models.ForeignKey(
+    users = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        related_name="bridges",
+        through='BridgeUpdate',
     )
-    bridge = models.ForeignKey(
-        Bridge,
-        on_delete=models.CASCADE,
+
+    objects = BridgeManager()
+
+    class Meta:
+        verbose_name = "bridge"
+        verbose_name_plural = "bridges"
+
+    def __str__(self):
+        return f"({self.latitude}, {self.longitude})"
+
+    @property
+    def bbox(self):
+        return (
+            f"bbox:{self.longitude_west},{self.latitude.south},"
+            f"{self.longitude_east},{self.latitude_north}"
+        )
+
+
+class BridgeUpdate(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="bridge_updates"
     )
-    update_date = models.DateTimeField(
-        auto_now_add=True,
-    )
+    bridge = models.ForeighKey('Bridge', related_name="bridge_updates")
+    updated_date = models.DateTimeField(auto_now_add=True)
